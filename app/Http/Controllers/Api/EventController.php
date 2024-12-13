bdc1f70be15a22afe07ad08986647e378788e727
esalasbauza <esalasbauza@gmail.com>
2024-12-13 15:29:39 +0100

    loading attendee relations

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -18,7 +18,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        $query = $this->loadRelationships(Event::query() );
+        $query = $this->loadRelationships(Event::query());
 
 
         return EventResource::collection($query->latest()->paginate());
