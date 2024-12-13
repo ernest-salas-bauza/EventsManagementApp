67bc227ba27b022e10065603b31b17e8a0f8cf41
esalasbauza <esalasbauza@gmail.com>
2024-12-13 15:57:17 +0100

    auth login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,4 +12,6 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update']);
+
+Route::post('/login', [AuthController::class,'login']);
 
