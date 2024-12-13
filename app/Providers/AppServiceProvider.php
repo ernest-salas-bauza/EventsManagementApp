49ab53fd2edf1f2777e1ccfed6b1fc99993e95c5
esalasbauza <esalasbauza@gmail.com>
2024-12-13 19:29:40 +0100

    rate limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //rate limiter
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(
 	            $request->user()?->id ?: $request->ip()
